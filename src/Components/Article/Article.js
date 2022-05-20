0296ba3014d5270476676ea88f4747e80faf09d0
simjoon <wesley0940@gmail.com>
2022-05-20 20:26:23 +0900

    tfjs4s

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -6,6 +6,7 @@ import { UrlContext } from "../../Context/UrlContext";
 import ReactPlayer from "react-player";
 import VideoSnapshot from 'video-snapshot';
 import * as tf from "@tensorflow/tfjs"
+import * as cvstfjs from "@microsoft/customvision-tfjs";
 
 const ContentsWrapper = styled.div`
     display: inline-flex;
@@ -45,9 +46,9 @@ const StyledVideo = styled.video`
     width: 100%;
     height 100%;
 `
-//const ModelUrl = "./tfjs_model/model.json"
+const ModelUrl = "tfjs_model/model.json"
 //const ModelUrl = "https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json"
-const ModelUrl = "./tfjs_tiny/model.json"
+//const ModelUrl = "./tfjs_tiny/model.json"
 
 function Article() {
     const { UrlData, SetUrlData} = useContext(UrlContext);
@@ -55,11 +56,16 @@ function Article() {
     const result = [];
     const result_ref = useRef([]);
     const video_ref = useRef();
+    let model;
+    let is_new_od_model;
     useEffect(()=>{
         UrlData != "" ? SetBlub(URL.createObjectURL(UrlData)) : SetBlub(Blub);
         console.log(UrlData, Blub)
     },[UrlData]);
-
+    function showProgress(percent) {
+        var pct = Math.floor(percent*100.0);
+        console.log(`${pct}% loaded`)
+    }
     function Img(props){
         function check(){
             console.log(props.k)
@@ -68,10 +74,91 @@ function Article() {
             <StyledImg ref = {el => (result_ref.current[props.k-1] = el)} onClick={check}  alt={props.k + "번 프레임 아직 설정하지않음"}></StyledImg>
         );
     }
-    const Predict = async () =>{
 
+    function _logistic(x) {
+        if (x > 0) {
+            return (1 / (1 + Math.exp(-x)));
+        } else {
+            const e = Math.exp(x);
+            return e / (1 + e);
+        }
+    }       
+    async function loadImage() {
+        console.log("Loading Img..");
+        const pixels = result_ref.current[1];
+        const input_size = model.inputs[0].shape[1]
+        let image = tf.browser.fromPixels(pixels,3);
+        image = tf.image.resizeBilinear(image.expandDims().toFloat(), [input_size, input_size]);
+        if (is_new_od_model) {
+            console.log( "Object Detection Model V2 detected." );
+            image = is_new_od_model ? image : image.reverse(-1); // RGB->BGR for old models
+        }
+        return image;
+
+    }    
+    const ANCHORS = [0.573, 0.677, 1.87, 2.06, 3.34, 5.47, 7.88, 3.53, 9.77, 9.17];
+    const NEW_OD_OUTPUT_TENSORS = ['detected_boxes', 'detected_scores', 'detected_classes'];
+
+    async function  Predict_service(inputs){
+        const outputs = await model.executeAsync(inputs, is_new_od_model ? NEW_OD_OUTPUT_TENSORS : null);
+	    const arrays = !Array.isArray(outputs) ? outputs.array() : Promise.all(outputs.map(t => t.array()));
+	    let predictions = await arrays;
+
+        if (predictions.length != 3) {
+            console.log( "Post processing..." );
+            const num_anchor = ANCHORS.length / 2;
+            const channels = predictions[0][0][0].length;
+            const height = predictions[0].length;
+            const width = predictions[0][0].length;
+    
+            const num_class = channels / num_anchor - 5;
+    
+            let boxes = [];
+            let scores = [];
+            let classes = [];
+    
+            for (var grid_y = 0; grid_y < height; grid_y++) {
+                for (var grid_x = 0; grid_x < width; grid_x++) {
+                    let offset = 0;
+    
+                    for (var i = 0; i < num_anchor; i++) {
+                        let x = (_logistic(predictions[0][grid_y][grid_x][offset++]) + grid_x) / width;
+                        let y = (_logistic(predictions[0][grid_y][grid_x][offset++]) + grid_y) / height;
+                        let w = Math.exp(predictions[0][grid_y][grid_x][offset++]) * ANCHORS[i * 2] / width;
+                        let h = Math.exp(predictions[0][grid_y][grid_x][offset++]) * ANCHORS[i * 2 + 1] / height;
+    
+                        let objectness = tf.scalar(_logistic(predictions[0][grid_y][grid_x][offset++]));
+                        let class_probabilities = tf.tensor1d(predictions[0][grid_y][grid_x].slice(offset, offset + num_class)).softmax();
+                        offset += num_class;
+    
+                        class_probabilities = class_probabilities.mul(objectness);
+                        let max_index = class_probabilities.argMax();
+                        boxes.push([x - w / 2, y - h / 2, x + w / 2, y + h / 2]);
+                        scores.push(class_probabilities.max().dataSync()[0]);
+                        classes.push(max_index.dataSync()[0]);
+                    }
+                }
+            }
+    
+            boxes = tf.tensor2d(boxes);
+            scores = tf.tensor1d(scores);
+            classes = tf.tensor1d(classes);
+    
+            const selected_indices = await tf.image.nonMaxSuppressionAsync(boxes, scores, 10);
+            predictions = [await boxes.gather(selected_indices).array(), await scores.gather(selected_indices).array(), await classes.gather(selected_indices).array()];
+        }
+    
+        return predictions;
     }
 
+    async function Predict(){
+        console.log( "Loading model..." );
+        model = await tf.loadGraphModel(ModelUrl, {onProgress: showProgress});
+        is_new_od_model = model.inputs.length == 3;
+        console.log( "Model loaded." );
+        const inputs = loadImage();
+        const predictions = await Predict_service(inputs);
+    }
 
     const Getimg = async () => {
         try {
