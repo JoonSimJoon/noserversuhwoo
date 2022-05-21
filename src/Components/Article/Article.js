cdcd160ea834b8b8a9934fd385220f5256c430cc
simjoon <wesley0940@gmail.com>
2022-05-22 01:57:07 +0900

    be4 usecoco

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -87,9 +87,6 @@ function Article() {
         );
     }
 
-    const ANCHORS = [0.573, 0.677, 1.87, 2.06, 3.34, 5.47, 7.88, 3.53, 9.77, 9.17];
-    const NEW_OD_OUTPUT_TENSORS = ['detected_boxes', 'detected_scores', 'detected_classes'];
-
     async function Predict(){
         console.log( "Loading model..." );
         model = await tf.loadGraphModel(ModelUrl, {onProgress: showProgress});
@@ -106,57 +103,14 @@ function Article() {
             newArray = shapev;
         }
         tensor = tensor.reshape(newArray)
+        const outputs = await model.executeAsync(tensor);
+        console.log( "Post processing...");
+        for (const output of outputs) {
+            const predict_data = output.dataSync();
+            console.log(predict_data);
+        }
         
-        const outputs = await model.executeAsync(tensor, is_new_od_model ? NEW_OD_OUTPUT_TENSORS : null);
-        const arrays = !Array.isArray(outputs) ? outputs.array() : Promise.all(outputs.map(t => t.array()));
-	    let predictions = await arrays;
-        console.log( "Post processing...",predictions);
-		const num_anchor = ANCHORS.length / 2;
-		const channels = predictions[0][0].length;
-		const height = predictions.length;
-		const width = predictions[0].length;
-		const num_class = channels / num_anchor - 5;
-        console.log(channels,num_anchor,num_class,predictions);
-
-		let boxes = [];
-		let scores = [];
-		let classes = [];
-
-		for (var grid_y = 0; grid_y < height; grid_y++) {
-			for (var grid_x = 0; grid_x < width; grid_x++) {
-				let offset = 0;
-
-				for (var i = 0; i < num_anchor; i++) {
-					let x = (_logistic(predictions[grid_y][grid_x][offset++]) + grid_x) / width;
-					let y = (_logistic(predictions[grid_y][grid_x][offset++]) + grid_y) / height;
-					let w = Math.exp(predictions[grid_y][grid_x][offset++]) * ANCHORS[i * 2] / width;
-					let h = Math.exp(predictions[grid_y][grid_x][offset++]) * ANCHORS[i * 2 + 1] / height;
-
-					let objectness = tf.scalar(_logistic(predictions[grid_y][grid_x][offset++]));
-                    let no = Array.from(predictions[grid_y][grid_x]).slice(offset, offset + num_class);
-                    //console.log(offset, offset + num_class ,num_class, no)
-                    console.log(no)
-					let class_probabilities = tf.tensor1d(no).softmax();
-					offset += num_class;
-
-					class_probabilities = class_probabilities.mul(objectness);
-					let max_index = class_probabilities.argMax();
-					boxes.push([x - w / 2, y - h / 2, x + w / 2, y + h / 2]);
-					scores.push(class_probabilities.max().dataSync()[0]);
-					classes.push(max_index.dataSync()[0]);
-				}
-			}
-		}
-
-		boxes = tf.tensor2d(boxes);
-		scores = tf.tensor1d(scores);
-		classes = tf.tensor1d(classes);
-
-		const selected_indices = await tf.image.nonMaxSuppressionAsync(boxes, scores, 10);
-		predictions = [await boxes.gather(selected_indices).array(), await scores.gather(selected_indices).array(), await classes.gather(selected_indices).array()];
-
-
-        console.log(predictions)
+		
     }
 
     const Getimg = async () => {
